0f7aaef4ebebc9145495fd19016703521d09fc86
xiaobu <1172504039@qq.com>
2015-12-16 14:47:57 +0800

    Initial commit

---
+++ app/src/main/java/net/gfdz/com/physicslayoutdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package net.gfdz.com.physicslayoutdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * https://github.com/Jawnnypoo/PhysicsLayout
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
